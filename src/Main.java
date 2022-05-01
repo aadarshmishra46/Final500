86aef589dd6264f75a64424fd58329b3c55c4d9a
Mohnish Pathade <mohnishpathade@ITSs-MacBook-Pro.local>
2022-05-01 22:37:47 +0530

    PrintALLSubSeq

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,13 @@
+import string.PrintAllSubSeq;
 import string.ReverseString;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        String s="abc";
-        String s1=new String("abc");
-       final int a=10;
-       int b=40;
-        if(a==b)
-            System.out.println("true");
-        if(s==s1){
-            System.out.println("yes");
-        }
-        ReverseString reverseString=new ReverseString();
-        reverseString.reverseString(new char[] {'h','e','l','l','o'});
+//        ReverseString reverseString=new ReverseString();
+//        reverseString.reverseString(new char[] {'h','e','l','l','o'});
+        PrintAllSubSeq printAllSubSeq = new PrintAllSubSeq();
+        printAllSubSeq.printAllSubSeq("abc");
     }
 }
